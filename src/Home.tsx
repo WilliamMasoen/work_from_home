1893c7dd38985299029044668a7de088bad86148
JustinAlianto <63857586+JustinAlianto@users.noreply.github.com>
2023-10-13 21:14:20 -0400

    Add Nav + Codebase Restructuring

---
+++ src/Home.tsx
@@ -0,0 +1,14 @@
+import './styles/Home.css'
+import Nav from './components/Nav.tsx'
+// import Footer from './components/Footer.tsx'
+
+function Home() {
+  return (
+    <>
+        <div><Nav /></div>
+        {/* <div><Footer /></div>; */}
+    </>
+  )
+}
+
+export default Home;
---
+++ src/components/Nav.tsx
@@ -0,0 +1,19 @@
+import logo from '../assets/images/Logo.png'
+import '../styles/Home.css'
+
+function Nav() {
+  return (
+    <>
+      {/* Website Logo */}
+      <div>
+        <img src={logo} className="web-logo"/> 
+      </div>
+
+      {/* Navigation Buttons */}
+      <div>
+      </div>
+    </>
+  );
+}
+
+export default Nav;
