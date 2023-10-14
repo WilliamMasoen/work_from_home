51158de0e6824557b2796c97b70740fdce534ccf
JustinAlianto <63857586+JustinAlianto@users.noreply.github.com>
2023-10-14 14:57:38 -0400

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,14 +1,30 @@
-import './styles/Home.css'
-import Nav from './components/Nav.tsx'
-import Footer from './components/Footer.tsx'
+import "./styles/Home.css";
+import Nav from "./components/Nav.tsx";
+import Footer from "./components/Footer.tsx";
 
 function Home() {
+  const QuotesList = [
+    "Carpe Diem.",
+    "Quote2",
+    "Quote3",
+    "Quote4",
+    "Quote5",
+    "Quote6",
+  ];
+
   return (
     <>
-        <div><Nav /></div>
-        <div><Footer /></div>;
+      <div>
+        <Nav />
+      </div>
+      {/* Quote Carousel */}
+      <div></div>
+      <div>
+        <Footer />
+      </div>
+      ;
     </>
-  )
+  );
 }
 
 export default Home;
