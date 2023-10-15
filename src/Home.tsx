2662626c102986c28d1d04c5f3327531947b8ff7
airlock04 <william_vm@icloud.com>
2023-10-14 21:50:36 -0400

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,14 +1,29 @@
-import './styles/Home.css'
-import Nav from './components/Nav.tsx'
-import Footer from './components/Footer.tsx'
+import "./styles/Home.css";
+import Nav from "./components/Nav.tsx";
+import Footer from "./components/Footer.tsx";
 
 function Home() {
+  const QuotesList = [
+    "Carpe Diem",
+    "Quote 1",
+    "Quote 1",
+    "Quote 1",
+    "Quote 1",
+    "Quote 1",
+  ];
+
   return (
     <>
-        <div><Nav /></div>
-        <div><Footer /></div>;
+      <div>
+        <Nav />
+      </div>
+      <div></div>
+      <div>
+        <Footer />
+      </div>
+      ;
     </>
-  )
+  );
 }
 
 export default Home;
